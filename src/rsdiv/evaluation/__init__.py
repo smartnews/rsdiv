e999adb5fe5b8e89ede72fd8f671f4cce9470546
Jiayu Chang <jiayu.chang@smartnews.com>
2022-07-11 23:13:38 -0400

    Add relevance_metrics.py.

--- src/rsdiv/evaluation/__init__.py
+++ src/rsdiv/evaluation/__init__.py
@@ -1,5 +1,4 @@
 from .diversity_metrics import DiversityMetrics
+from .relevance_metrics import CosineRelevanceMetric, InnerProductRelevanceMetric
 
-__all__ = [
-    "DiversityMetrics",
-]
+__all__ = ["DiversityMetrics", "CosineRelevanceMetric", "InnerProductRelevanceMetric"]
