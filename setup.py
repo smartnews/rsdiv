01c32c5170e74cd5e70bfdcddef86f66c1ffb1bf
yinsn <102125674+yinsn@users.noreply.github.com>
2022-07-13 23:21:44 +0800

    add requirements (#27)

--- setup.py
+++ setup.py
@@ -7,10 +7,20 @@ SETUP_DIRECTORY = Path(__file__).resolve().parent
 with (SETUP_DIRECTORY / "README.md").open() as ifs:
     LONG_DESCRIPTION = ifs.read()
 
+install_requires = (
+    [
+        "numpy>=1.23.1",
+        "pandas>=1.4.3",
+        "scipy>=1.8.1",
+        "lightfm>=1.16",
+        "scikit-learn>=1.1.1",
+        "matplotlib>=3.5.2",
+    ],
+)
 
 setup(
     name="rsdiv",
-    version="0.1.6",
+    version="0.1.7",
     author="Yin Cheng",
     author_email="yin.sjtu@gmail.com",
     long_description=LONG_DESCRIPTION,
@@ -19,5 +29,6 @@ setup(
     python_requires=">=3.6",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    install_requires=install_requires,
     include_package_data=True,
 )
