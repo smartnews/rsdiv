39ed458a8d6ced43ee4badbb055cecb6a71000f1
Jiayu Chang <JyChang012@outlook.com>
2022-07-28 11:01:41 -0700

    Add test for SSD.

---
+++ test/recommenders/test_ssd.py
@@ -0,0 +1,88 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+from pytest import fixture, mark, raises
+
+import rsdiv as rs
+
+
+class TestSlidingSpectrumDecomposition:
+    @fixture
+    def candidate_scale(self) -> int:
+        return 200
+
+    @fixture
+    def dataset(self, candidate_scale: int) -> pd.DataFrame:
+        loader = rs.MovieLens1MDownLoader()
+        dataframe = loader.read_items()
+        return dataframe.head(candidate_scale).copy()
+
+    @fixture
+    def embedder(self) -> rs.FastTextEmbedder:
+        return rs.FastTextEmbedder()
+
+    @fixture
+    def relevance_scores(self, candidate_scale: int) -> np.ndarray:
+        rng = np.random.default_rng(96)
+        scores = -np.sort(-rng.random(candidate_scale))
+        return scores
+
+    @fixture
+    def embeddings(
+        self, dataset: pd.DataFrame, embedder: rs.FastTextEmbedder
+    ) -> np.ndarray:
+        dataframe = dataset
+        dataframe["embedding"] = dataframe["genres"].apply(embedder.embedding_list)
+        embedding: np.ndarray = np.asarray(list(dataframe["embedding"]))
+
+        return embedding
+
+    # Note: these tests calculate diversity scores of a recommendation by movies' categorical genres, but the diversity
+    # enhancement algorithms use genres' embeddings from fasttext to rerank. As a result, the scores might decrease when
+    # gamma or scale are small. The tests here choose scale and gamma large enough to make SSD pass.
+    @mark.parametrize("scale", [50, 70, 100])
+    @mark.parametrize("gamma", np.linspace(20, 60, 20))
+    def test_rerank_scale(
+        self,
+        dataset: pd.DataFrame,
+        relevance_scores: np.ndarray,
+        embeddings: np.ndarray,
+        scale: int,
+        gamma: float,
+    ) -> None:
+        genres = dataset["genres"]
+        ssd = rs.SlidingSpectrumDecomposition(gamma)
+        metrics = rs.DiversityMetrics()
+        selected_ind = ssd.rerank(relevance_scores, scale, embeddings=embeddings)
+        gini_org = metrics.gini_coefficient(genres[:scale])
+        gini_ssd = metrics.gini_coefficient([genres[index] for index in selected_ind])
+        assert selected_ind[0] == 0
+        assert len(selected_ind) == scale
+        print("\norg v.s. ssd: ", gini_org, gini_ssd)
+        assert gini_org >= gini_ssd
+
+    @mark.parametrize("scale", [100, 120, 150, 170])
+    def test_rerank_lambda(
+        self,
+        dataset: pd.DataFrame,
+        relevance_scores: np.ndarray,
+        embeddings: np.ndarray,
+        scale: int,
+        gamma: List[float] = np.linspace(0, 20, 20),
+    ) -> None:
+        genres = dataset["genres"]
+        metrics = rs.DiversityMetrics()
+        gini_org = metrics.gini_coefficient(genres[:scale])
+        gini_ans = []
+        for lbd in gamma:
+            ssd = rs.SlidingSpectrumDecomposition(lbd)
+            selected_ind = ssd.rerank(relevance_scores, scale, embeddings=embeddings)
+            gini_ssd = metrics.gini_coefficient(
+                [genres[index] for index in selected_ind]
+            )
+            gini_ans.append(gini_ssd)
+        print(gini_ans)
+        assert selected_ind[0] == 0
+        assert gini_ans[0] == gini_org
+        assert sorted(gini_ans, reverse=True) == gini_ans
