487fba52eea8e035e7f8913b12eb2f0f6aeeac00
yin.cheng <yincheng@SNC0135.local>
2022-07-30 21:35:31 +0800

    mypy fix ssd.py

--- src/rsdiv/diversity/ssd.py
+++ src/rsdiv/diversity/ssd.py
@@ -17,25 +17,25 @@ class SlidingSpectrumDecomposition(BaseReranker):
         quality_scores: np.ndarray,
         k: int,
         *,
-        similarity_scores: None = None,
-        embeddings: np.ndarray,
+        similarity_scores: Optional[np.ndarray] = None,
+        embeddings: Optional[np.ndarray],
         inplace: bool = False
     ) -> Sequence[int]:
         assert k > 0, "k must be larger than 0!"
-        if not inplace:
-            embeddings = embeddings.copy()
         selection = np.argmax(quality_scores).item()
         ret = [selection]
-        volume = self.gamma * norm(embeddings[selection])
-        for _ in range(k - 1):
-            selected_emb = embeddings[selection]
-            selected_emb /= norm(selected_emb)
-            embeddings -= np.outer(embeddings @ selected_emb, selected_emb)
-            norms = norm(embeddings, axis=1)
-            norms *= volume
-            scores = norms + quality_scores
-            scores[ret] = -np.inf
-            selection = np.argmax(scores)
-            ret.append(selection)
-            volume = norms[selection]
+        if not inplace and embeddings:
+            embeddings = embeddings.copy()
+            volume = self.gamma * norm(embeddings[selection])
+            for _ in range(k - 1):
+                selected_emb = embeddings[selection]
+                selected_emb /= norm(selected_emb)
+                embeddings -= np.outer(embeddings @ selected_emb, selected_emb)
+                norms = norm(embeddings, axis=1)
+                norms *= volume
+                scores = norms + quality_scores
+                scores[ret] = -np.inf
+                selection = int(np.argmax(scores))
+                ret.append(selection)
+                volume = norms[selection]
         return ret
