b406fe6bfab2af1fde4c61b951fe0726c5801771
Jiayu Chang <40067028+JyChang012@users.noreply.github.com>
2022-08-03 17:10:48 -0400

    Add ranking metrics. (#41)

---
+++ test/evaluation/test_map.py
@@ -0,0 +1,88 @@
+import numpy as np
+from numpy.testing import assert_allclose
+from pytest import mark, skip
+
+from rsdiv.evaluation import RankingMetrics
+
+
+class TestMAP:
+    rng = np.random.default_rng(42)
+
+    @mark.parametrize("user_size", [1, 10, 50, 100, 500])
+    @mark.parametrize("recommend_size", [10, 50, 100, 500])
+    @mark.parametrize("position", [10, 50, 100, 500])
+    def test_range(self, user_size: int, recommend_size: int, position: int) -> None:
+        if position > recommend_size:
+            skip()
+        recommend_relevance = self.rng.integers(
+            0,
+            2,
+            (user_size, recommend_size) if user_size != 1 else recommend_size,
+            dtype=int,
+        )
+        assert (
+            0
+            <= RankingMetrics.mean_average_precision(recommend_relevance, position)
+            <= 1
+        )
+
+    @mark.parametrize("user_size", [1, 10, 50, 100, 500])
+    @mark.parametrize("recommend_size", [10, 50, 100, 500])
+    @mark.parametrize("position", [10, 50, 100, 500])
+    def test_all_ones(self, user_size: int, recommend_size: int, position: int) -> None:
+        if position > recommend_size:
+            skip()
+        recommend_relevance = np.ones(
+            (user_size, recommend_size) if user_size != 1 else recommend_size,
+            dtype=int,
+        )
+        assert_allclose(
+            RankingMetrics.mean_average_precision(recommend_relevance, position), 1
+        )
+
+    @mark.parametrize("user_size", [1, 10, 50, 100, 500])
+    @mark.parametrize("recommend_size", [10, 50, 100, 500])
+    @mark.parametrize("position", [10, 50, 100, 500])
+    def test_all_zeros(
+        self, user_size: int, recommend_size: int, position: int
+    ) -> None:
+        if position > recommend_size:
+            skip()
+        recommend_relevance = np.zeros(
+            (user_size, recommend_size) if user_size != 1 else recommend_size,
+            dtype=int,
+        )
+        assert_allclose(
+            RankingMetrics.mean_average_precision(recommend_relevance, position), 0
+        )
+
+    @mark.parametrize("user_size", [10, 50, 100, 500])
+    @mark.parametrize("recommend_size", [10, 50, 100, 500])
+    def test_random_swap(self, user_size: int, recommend_size: int) -> None:
+        recommend_relevance = self.rng.integers(
+            0,
+            2,
+            (user_size, recommend_size) if user_size != 1 else recommend_size,
+            dtype=int,
+        )
+        row = self.rng.integers(0, (user_size, recommend_size), 1, dtype=int)
+        old_map = RankingMetrics.mean_average_precision(
+            recommend_relevance, recommend_size
+        )
+        arg_ones_ind = np.argwhere(recommend_relevance[row].flatten() == 1).flatten()
+        arg_zeros_ind = np.argwhere(recommend_relevance[row].flatten() == 0).flatten()
+
+        first_one = arg_ones_ind[0]
+        first_zero = arg_zeros_ind[0]
+
+        recommend_relevance[row, [first_zero, first_one]] = recommend_relevance[
+            row, [first_one, first_zero]
+        ]
+        new_map = RankingMetrics.mean_average_precision(
+            recommend_relevance, recommend_size
+        )
+
+        if first_one < first_zero:
+            assert new_map < old_map
+        else:
+            assert new_map > old_map
---
+++ test/evaluation/test_nDCG.py
@@ -0,0 +1,86 @@
+from typing import Dict, List, Sequence
+
+import numpy as np
+from numpy.testing import assert_allclose
+from pytest import mark, skip
+
+from rsdiv.evaluation import RankingMetrics
+
+
+class TestNDCG:
+    rng = np.random.default_rng(42)
+
+    @mark.parametrize("item_size", [10, 100, 500])
+    @mark.parametrize("relevant_size", [10, 100, 500])
+    @mark.parametrize("position", [10, 100, 500])
+    @mark.parametrize("exponential", [True, False])
+    def test_equality(
+        self, item_size: int, relevant_size: int, position: int, exponential: bool
+    ) -> None:
+        if position > min(item_size, relevant_size) or relevant_size > item_size:
+            skip()
+        scores = self.rng.random(item_size)
+        sorted_indices = np.argsort(scores)
+        sorted_indices = sorted_indices[: -relevant_size - 1 : -1]
+        relevant_id: List[int] = sorted_indices.tolist()
+        assert_allclose(
+            RankingMetrics.nDCG(
+                scores.__getitem__, relevant_id, relevant_id, position, exponential
+            ),
+            1,
+        )
+
+    @mark.parametrize("item_size", [10, 100, 500])
+    @mark.parametrize("relevant_size", [10, 100, 500])
+    @mark.parametrize("recommend_size", [10, 100, 500])
+    @mark.parametrize("position", [10, 100, 500])
+    @mark.parametrize("exponential", [True, False])
+    def test_range(
+        self,
+        item_size: int,
+        relevant_size: int,
+        recommend_size: int,
+        position: int,
+        exponential: bool,
+    ) -> None:
+        if (
+            position > min([item_size, relevant_size, recommend_size])
+            or not max(relevant_size, recommend_size) <= item_size
+        ):
+            skip()
+        scores = self.rng.random(item_size)
+        sorted_indices = np.argsort(scores)
+        relevant_ind = sorted_indices[: -relevant_size - 1 : -1].tolist()
+        self.rng.shuffle(relevant_ind)
+        recommend_id = self.rng.choice(
+            np.arange(item_size), recommend_size, replace=False
+        ).tolist()
+        assert (
+            0
+            <= RankingMetrics.nDCG(
+                scores.__getitem__, relevant_ind, recommend_id, position, exponential
+            )
+            <= 1
+        )
+
+    @mark.parametrize("position", range(1, 4))
+    @mark.parametrize("exponential", [True, False])
+    def test_no_relevant(
+        self,
+        position: int,
+        exponential: bool,
+        relevant_ind: Sequence[int] = (11, 7, 2, 3),
+        recommend_ind: Sequence[int] = (4, 9, 8, 2, 1, 90),
+    ) -> None:
+        scores = self.rng.random(len(relevant_ind))
+        item2score: Dict[int, float] = dict(zip(relevant_ind, scores))
+        assert_allclose(
+            RankingMetrics.nDCG(
+                lambda idx: item2score.get(idx, 0),
+                relevant_ind,
+                recommend_ind,
+                position,
+                exponential,
+            ),
+            0,
+        )
