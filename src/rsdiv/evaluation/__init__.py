8a7bbf38087c3552257646e2804c84fbf9fe455b
yin.cheng <yin.cheng@smartnews.com>
2022-09-24 22:22:44 +0800

    add the distance for two ordered lists

--- src/rsdiv/evaluation/__init__.py
+++ src/rsdiv/evaluation/__init__.py
@@ -1,10 +1,12 @@
 from .diversity_metrics import DiversityMetrics
+from .ranking_distance import RankingDistance
 from .ranking_metrics import RankingMetrics
 from .relevance_metrics import CosineRelevanceMetric, InnerProductRelevanceMetric
 
 __all__ = [
     "DiversityMetrics",
+    "RankingDistance",
+    "RankingMetrics",
     "CosineRelevanceMetric",
     "InnerProductRelevanceMetric",
-    "RankingMetrics",
 ]
---
+++ src/rsdiv/evaluation/ranking_distance.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import numpy as np
+
+
+class RankingDistance:
+    """Distance between ordered lists."""
+
+    @classmethod
+    def common_at_k(cls, source_list: List, target_list: List, truncate_at: int) -> int:
+        """Calculate the common elements for two ordered lists.
+
+        Args:
+            source_list (List): First ordered list.
+            target_list (List): Second ordered list.
+            truncate_at (int): Until which position the numbers would be considered.
+
+        Returns:
+            int: The number of common elements.
+        """
+        source_set = set(source_list[:truncate_at])
+        target_set = set(target_list[:truncate_at])
+        return len(source_set.intersection(target_set))
+
+    @classmethod
+    def average_common_at_k(
+        cls, source_list: List, target_list: List, truncate_at: int
+    ) -> float:
+        """Calculate the average common elements for two ordered lists.
+
+        Args:
+            source_list (List): First ordered list.
+            target_list (List): Second ordered list.
+            truncate_at (int): Until which position the numbers would be considered.
+
+        Returns:
+            float: The average number of common elements.
+        """
+        average_common: List = []
+        for num in range(1, truncate_at + 1):
+            average_common.append(cls.common_at_k(source_list, target_list, num))
+        return float(np.mean(average_common))
