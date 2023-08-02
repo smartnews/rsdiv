fe1b79fcb1236ba1e14f139215e82d2075488bdf
Stanislav Filin <stasfilin@hotmail.com>
2023-08-02 14:41:51 +0000

    Change dataset, add lru_cache. Starting to use pathlib
    Signed-off-by: Stanislav Filin <stasfilin@hotmail.com>

--- src/rsdiv/dataset/movielens_100k.py
+++ src/rsdiv/dataset/movielens_100k.py
@@ -1,7 +1,7 @@
-import os
-from typing import List
-
 import pandas as pd
+from pathlib import Path
+from typing import List
+from functools import lru_cache
 
 from .base import BaseDownloader
 
@@ -10,20 +10,21 @@ class MovieLens100KDownLoader(BaseDownloader):
     """MovieLens dataset downLoader for 100K interactions."""
 
     DOWNLOAD_URL: str = "http://files.grouplens.org/datasets/movielens/ml-100k.zip"
-    DEFAULT_PATH: str = os.path.join(os.getcwd(), "ml-100k")
+    DEFAULT_PATH: str = str(Path.cwd() / "ml-100k")
 
+    @lru_cache(maxsize=1)
     def read_ratings(self) -> pd.DataFrame:
-        ratings_path: str = os.path.join(self.DEFAULT_PATH, "u.data")
+        ratings_path: Path = Path(self.DEFAULT_PATH) / "u.data"
         df_ratings: pd.DataFrame = pd.read_csv(
             ratings_path, sep="\t", header=None, engine="python"
-        ).copy()
+        )
         df_ratings.columns = pd.Index(["userId", "movieId", "rating", "timestamp"])
         df_ratings["timestamp"] = pd.to_datetime(df_ratings.timestamp, unit="s")
-
         return df_ratings
 
+    @lru_cache(maxsize=1)
     def read_users(self) -> pd.DataFrame:
-        users_path: str = os.path.join(self.DEFAULT_PATH, "u.user")
+        users_path: Path = Path(self.DEFAULT_PATH) / "u.user"
         df_users: pd.DataFrame = pd.read_csv(
             users_path,
             sep="|",
@@ -31,17 +32,17 @@ class MovieLens100KDownLoader(BaseDownloader):
             engine="python",
             names=["userId", "age", "gender", "occupation", "zipcode"],
         )
-
-        return df_users[["userId", "gender", "age", "occupation", "zipcode"]]
+        return df_users
 
     def _read_genres(self) -> List[str]:
-        genres_path: str = os.path.join(self.DEFAULT_PATH, "u.genre")
-        with open(genres_path, "r") as outfile:
+        genres_path: Path = Path(self.DEFAULT_PATH) / "u.genre"
+        with genres_path.open() as outfile:
             genres = outfile.read()
             return [pair.split("|")[0] for pair in genres.split("\n")][:-2]
 
+    @lru_cache(maxsize=1)
     def read_items(self) -> pd.DataFrame:
-        movies_path: str = os.path.join(self.DEFAULT_PATH, "u.item")
+        movies_path: Path = Path(self.DEFAULT_PATH) / "u.item"
         genres: List[str] = self._read_genres()
         df_items: pd.DataFrame = pd.read_csv(
             movies_path,
@@ -53,7 +54,7 @@ class MovieLens100KDownLoader(BaseDownloader):
             + genres,
         )
         df_items["title"] = df_items["title"].str[:-7]
-        df_items["title"] = df_items["title"].apply(lambda x: x.split(",")[0])
+        df_items["title"] = df_items["title"].apply(lambda x: x.split(",")[0] if "," in x else x)
         df_items["release_date"] = pd.to_datetime(df_items.release_date)
         df_items["genres"] = df_items[genres] @ (df_items[genres].columns + "|")
         df_items["genres"] = df_items["genres"].apply(lambda x: x[:-1].split("|"))
--- src/rsdiv/dataset/movielens_1m.py
+++ src/rsdiv/dataset/movielens_1m.py
@@ -1,6 +1,6 @@
-import os
-
 import pandas as pd
+from pathlib import Path
+from functools import lru_cache
 
 from .base import BaseDownloader
 
@@ -9,20 +9,21 @@ class MovieLens1MDownLoader(BaseDownloader):
     """MovieLens dataset downLoader for 1M interactions."""
 
     DOWNLOAD_URL: str = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
-    DEFAULT_PATH: str = os.path.join(os.getcwd(), "ml-1m")
+    DEFAULT_PATH: str = str(Path.cwd() / "ml-1m")
 
+    @lru_cache(maxsize=1)
     def read_ratings(self) -> pd.DataFrame:
-        ratings_path: str = os.path.join(self.DEFAULT_PATH, "ratings.dat")
+        ratings_path: Path = Path(self.DEFAULT_PATH) / "ratings.dat"
         df_ratings: pd.DataFrame = pd.read_csv(
             ratings_path, sep="::", header=None, engine="python"
-        ).copy()
+        )
         df_ratings.columns = pd.Index(["userId", "movieId", "rating", "timestamp"])
         df_ratings["timestamp"] = pd.to_datetime(df_ratings.timestamp, unit="s")
-
         return df_ratings
 
+    @lru_cache(maxsize=1)
     def read_users(self) -> pd.DataFrame:
-        users_path: str = os.path.join(self.DEFAULT_PATH, "users.dat")
+        users_path: Path = Path(self.DEFAULT_PATH) / "users.dat"
         df_users: pd.DataFrame = pd.read_csv(
             users_path,
             sep="::",
@@ -30,11 +31,11 @@ class MovieLens1MDownLoader(BaseDownloader):
             engine="python",
             names=["userId", "gender", "age", "occupation", "zipcode"],
         )
-
         return df_users
 
+    @lru_cache(maxsize=1)
     def read_items(self) -> pd.DataFrame:
-        movies_path: str = os.path.join(self.DEFAULT_PATH, "movies.dat")
+        movies_path: Path = Path(self.DEFAULT_PATH) / "movies.dat"
         df_items: pd.DataFrame = pd.read_csv(
             movies_path,
             sep="::",
@@ -45,7 +46,6 @@ class MovieLens1MDownLoader(BaseDownloader):
         )
         df_items["release_date"] = df_items["title"].str[-5:-1].astype("int")
         df_items["title"] = df_items["title"].str[:-7]
-        df_items["title"] = df_items["title"].apply(lambda x: x.split(",")[0])
+        df_items["title"] = df_items["title"].apply(lambda x: x.split(",")[0] if "," in x else x)
         df_items["genres"] = df_items["genres"].apply(lambda x: x.split("|"))
-
         return df_items
