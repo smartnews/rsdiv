a747a090e5d042e6c6b79fbfaaf34b3dd978d8e2
Stanislav Filin <stasfilin@hotmail.com>
2023-08-02 14:47:13 +0000

    Update diversity
    Signed-off-by: Stanislav Filin <stasfilin@hotmail.com>

--- src/rsdiv/diversity/ssd.py
+++ src/rsdiv/diversity/ssd.py
@@ -14,6 +14,12 @@ class SlidingSpectrumDecomposition(BaseReranker):
         assert gamma >= 0, "gamma should be >= 0!"
         self.gamma = gamma
 
+    def _adjust_embeddings(self, embeddings: np.ndarray, selected_emb: np.ndarray):
+        selected_norm = norm(selected_emb)
+        if selected_norm > 1e-7:  # treat new selection as 0 vector if it's too small
+            selected_emb /= selected_norm
+            np.subtract(embeddings, np.outer(embeddings @ selected_emb, selected_emb), out=embeddings)
+
     def rerank(
         self,
         quality_scores: np.ndarray,
@@ -24,22 +30,17 @@ class SlidingSpectrumDecomposition(BaseReranker):
         inplace: bool = False
     ) -> Sequence[int]:
         assert k > 0, "k must be larger than 0!"
-        if not inplace:
-            embeddings = embeddings.copy()
         selection = np.argmax(quality_scores).item()
         ret = [selection]
         volume = self.gamma * norm(embeddings[selection])
+        norms = norm(embeddings, axis=1)
         for _ in range(k - 1):
             selected_emb = embeddings[selection]
-            selected_norm = norm(selected_emb)
-            if (
-                selected_norm > 1e-7
-            ):  # treat new selection as 0 vector if it's too small
-                selected_emb /= selected_norm
-                embeddings -= np.outer(embeddings @ selected_emb, selected_emb)
-            norms = norm(embeddings, axis=1)
-            norms *= volume
-            scores = norms + quality_scores
+            self._adjust_embeddings(embeddings, selected_emb)
+            norms = norm(embeddings, axis=1)  # update norms after adjusting embeddings
+            scaled_norms = norms.copy()
+            scaled_norms *= volume
+            scores = scaled_norms + quality_scores
             scores[ret] = -np.inf
             selection = np.argmax(scores).item()
             ret.append(selection)
